0d039f0c2acdd8aeedaa9344dbb940cababba5fd
Hemin Johnson <achayan1992@gmail.com>
2019-09-01 13:57:45 +0200

    feat: add cart icon component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,9 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { auth } from '../../firebase/firebase.utils'
-
 import { ReactComponent as Logo } from '../../assets/crown.svg'
+import CartIcon from '../cart-icon/cart-icon.component'
+
 import './header.styles.scss'
 
 const Header = ({currentUser}) => (
@@ -29,6 +30,7 @@ const Header = ({currentUser}) => (
             SIGN IN
           </Link>
       }
+      <CartIcon />
     </div>
   </div>
 )
