2baf405606f40ec626e6edeb2e5dc454655dd141
Hemin Johnson <achayan1992@gmail.com>
2019-09-01 14:06:25 +0200

    feat: add cart dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import { auth } from '../../firebase/firebase.utils'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropDown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss'
 
@@ -32,6 +33,7 @@ const Header = ({currentUser}) => (
       }
       <CartIcon />
     </div>
+    <CartDropDown />
   </div>
 )
 
